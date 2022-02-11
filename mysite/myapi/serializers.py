37e5b978da011ab44a806e4566c106abbe3d3cdd
Isaque Hollanda Gonçalves <77072103+Isaquehg@users.noreply.github.com>
2022-02-11 09:01:58 -0300

    REST API

---
+++ mysite/myapi/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Hero
+
+#Here we are converting JSON to a database model and reciprocally
+class HeroSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Hero
+        fields = ('name', 'alias')
